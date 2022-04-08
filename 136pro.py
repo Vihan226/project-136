f92569214296c254cfeaf20edd7fc88d574421f2
Vihan226 <71296969+Vihan226@users.noreply.github.com>
2022-04-08 18:09:47 -0400

    Add files via upload

--- 136pro.py
+++ 136pro.py
@@ -1,13 +1,17 @@
 from flask import Flask, jsonify, request
-
+from data import data
 app= Flask(__name__)
 @app.route("/")
 
 def index():
-    return 'index';
+    return jsonify({'data':data, 'message':'success'}), 200
 
-@app.route("/planet")
-def planet():
+
+@app.route("/star")
+def star():
+    name= request.args.get('name')
+    star_data= next(item for item in data if item['name']==name)
+    return jsonify({'data':star_data, 'message':'success'}), 200
     
 
 
